39a865931a0b9ec93bdcfad3bb414a3cfb5bc311
elcoosp <elcoosp@gmail.com>
2026-04-20 00:13:15 +0200

    feat(floem): complete ReactiveContext implementation

--- quoin-floem/src/lib.rs
+++ quoin-floem/src/lib.rs
@@ -1,4 +1,4 @@
-use floem_reactive::{ReadSignal, RwSignal};
+use floem_reactive::{ReadSignal, RwSignal, SignalGet, SignalWith};
 use quoin::{Executor, JoinHandle, ReactiveContext, Signal};
 use send_wrapper::SendWrapper;
 use std::future::Future;
@@ -18,15 +18,15 @@ impl ReactiveContext for FloemContext {
     type Executor = FloemExecutor;
 
     fn create_signal<T: Clone + 'static>(&self, initial: T) -> Self::Signal<T> {
-        todo!()
+        FloemSignal::new(initial)
     }
 
     fn executor(&self) -> Self::Executor {
-        todo!()
+        FloemExecutor
     }
 
     fn request_update(&self) {
-        todo!()
+        // Floem's reactivity is automatic via signal subscriptions.
     }
 }
 
@@ -35,13 +35,23 @@ pub struct FloemSignal<T: Clone + 'static> {
     value: ReadSignal<SendWrapper<T>>,
 }
 
+impl<T: Clone + 'static> FloemSignal<T> {
+    fn new(initial: T) -> Self {
+        let rw_signal = RwSignal::new(SendWrapper::new(initial));
+        Self {
+            value: rw_signal.read_only(),
+        }
+    }
+}
+
 impl<T: Clone + 'static> Signal<T> for FloemSignal<T> {
     fn get(&self) -> T {
-        todo!()
+        // SignalGet::get() returns SendWrapper<T>, then we deref and clone
+        (*self.value.get()).clone()
     }
 
     fn with<U>(&self, f: impl FnOnce(&T) -> U) -> U {
-        todo!()
+        self.value.with(|wrapper| f(&**wrapper))
     }
 }
 
@@ -56,7 +66,14 @@ impl Executor for FloemExecutor {
         F: Future + Send + 'static,
         F::Output: Send + 'static,
     {
-        todo!()
+        let (tx, rx) = futures::channel::oneshot::channel();
+
+        std::thread::spawn(move || {
+            let result = futures::executor::block_on(future);
+            let _ = tx.send(result);
+        });
+
+        FloemJoinHandle { rx: Some(rx) }
     }
 }
 
@@ -66,4 +83,19 @@ pub struct FloemJoinHandle<T> {
 
 impl<T: Send + 'static> JoinHandle<T> for FloemJoinHandle<T> {
     fn abort(&self) {}
+}
+
+impl<T: Send + 'static> std::future::IntoFuture for FloemJoinHandle<T> {
+    type Output = Result<T, futures::channel::oneshot::Canceled>;
+    type IntoFuture = Pin<Box<dyn Future<Output = Self::Output> + Send>>;
+
+    fn into_future(mut self) -> Self::IntoFuture {
+        Box::pin(async move {
+            if let Some(rx) = self.rx.take() {
+                rx.await
+            } else {
+                unreachable!("Receiver should be set")
+            }
+        })
+    }
 }
