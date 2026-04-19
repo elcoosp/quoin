db0aca86f393d77396c61428f07fe5086cdf74fd
elcoosp <elcoosp@gmail.com>
2026-04-20 00:21:02 +0200

    test: add conformance tests for xilem and floem adapters

---
+++ quoin-xilem/tests/conformance.rs
@@ -0,0 +1,73 @@
+use quoin::{Executor, ReactiveContext};
+use quoin_conformance::{define_conformance_tests, SleepExt};
+use quoin_xilem::{XilemContext, XilemExecutor};
+use std::future::Future;
+use std::time::Duration;
+
+#[derive(Clone)]
+struct TestExecutor(XilemExecutor);
+
+impl SleepExt for TestExecutor {
+    fn sleep(&self, duration: Duration) -> impl Future<Output = ()> + Send {
+        futures_timer::Delay::new(duration)
+    }
+}
+
+impl Executor for TestExecutor {
+    type JoinHandle<T: Send + 'static> = <XilemExecutor as Executor>::JoinHandle<T>;
+
+    fn spawn<F>(&self, future: F) -> Self::JoinHandle<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static,
+    {
+        self.0.spawn(future)
+    }
+}
+
+struct TestHarness {
+    context: XilemContext,
+}
+
+impl Clone for TestHarness {
+    fn clone(&self) -> Self {
+        Self::new()
+    }
+}
+
+impl TestHarness {
+    fn new() -> Self {
+        Self {
+            context: XilemContext::new(),
+        }
+    }
+}
+
+impl ReactiveContext for TestHarness {
+    type Signal<T: Clone + 'static> = <XilemContext as ReactiveContext>::Signal<T>;
+    type Executor = TestExecutor;
+
+    fn create_signal<T: Clone + 'static>(&self, initial: T) -> Self::Signal<T> {
+        self.context.create_signal(initial)
+    }
+
+    fn executor(&self) -> Self::Executor {
+        TestExecutor(self.context.executor())
+    }
+
+    fn request_update(&self) {
+        self.context.request_update()
+    }
+}
+
+impl quoin_conformance::TestContextProvider for TestHarness {
+    fn setup_context() -> Self {
+        Self::new()
+    }
+
+    fn block_on<F: Future>(future: F) -> F::Output {
+        futures::executor::block_on(future)
+    }
+}
+
+define_conformance_tests!(sync, TestHarness);
