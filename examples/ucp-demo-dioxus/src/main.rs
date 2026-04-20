6ff1cc511f15caacf0affe253b35e9e94127eb80
elcoosp <elcoosp@gmail.com>
2026-04-20 23:28:26 +0200

    fix: working tailwind style dioxus

--- examples/ucp-demo-dioxus/src/main.rs
+++ examples/ucp-demo-dioxus/src/main.rs
@@ -1,6 +1,25 @@
 // examples/ucp-demo-dioxus/src/main.rs
+use dioxus::desktop::Config;
 use ucp_lib::DemoApp;
 
 fn main() {
-    dioxus::launch(DemoApp);
+    let tailwind_head = r#"
+        <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            html, body, #main {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                width: 100%;
+                overflow: hidden;
+            }
+        </style>
+    "#
+    .to_string();
+
+    let cfg = Config::default().with_custom_head(tailwind_head);
+
+    dioxus::LaunchBuilder::desktop()
+        .with_cfg(cfg)
+        .launch(DemoApp);
 }
