472d33c65467bfaefd049c7bf815ca2d57e3bbf6
elcoosp <elcoosp@gmail.com>
2026-04-21 11:04:43 +0200

    fix: cond compile macro mods

--- quoin-macros/src/emit/mod.rs
+++ quoin-macros/src/emit/mod.rs
@@ -1,6 +1,13 @@
-pub mod gpui;
-pub mod leptos;
+#[cfg(feature = "dioxus")]
 pub mod dioxus;
-pub mod render_gpui;
-pub mod render_leptos;
+#[cfg(feature = "gpui")]
+pub mod gpui;
+#[cfg(feature = "leptos")]
+pub mod leptos;
+
+#[cfg(feature = "dioxus")]
 pub mod render_dioxus;
+#[cfg(feature = "gpui")]
+pub mod render_gpui;
+#[cfg(feature = "leptos")]
+pub mod render_leptos;
